8e40b956829f642eb3b10b24e8f24e8b17d8e68d
Ana Finotelli <finotelli2004@gmail.com>
2022-12-10 22:56:42 -0300

    Groot

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string FraseParaSair = "tchau";
+
+Console.WriteLine("--- Groot Chatbot ---\n");
+
+string pergunta;
+
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+
+    string resposta = ObtemRespostaGroot(pergunta);
+
+    MostraResposta(ObtemRespostaGroot(pergunta));
+
+}
+while (pergunta != FraseParaSair);
+
+void MostraResposta(string resposta)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"Resposta: {resposta}");
+    Console.ResetColor();
+}
+
+string ObtemRespostaGroot(string pergunta)
+{
+    return pergunta == FraseParaSair ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
